68eae328759d279df2dd8d151189d691c7f2ddc7
Arwa Ali <aaa297@cornell.edu>
2021-09-29 01:18:41 -0400

    added fade in/ out effect to welcome quote

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,25 +1,40 @@
 import { useSelector } from 'react-redux';
 import { Grid, CircularProgress, Typography } from '@material-ui/core';
+import Fade from '@mui/material/Fade';
 import Post from './Post/Post'
 import useStyles from './styles'
+import Hidden from '@material-ui/core/Hidden';
 
 const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
     return (
-        !posts.length ?
-            <div className={classes.quoteWrapper}>
-                <Typography className={classes.quote} variant='h3' align='center'>Art enables us to find ourselves and lose ourselves at the same time.” – Thomas Merton</Typography>
-            </div> :
-            (<Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
-                    <Grid key={post._id} item xs={12} sm={6}>
-                        <Post post={post} setCurrentId={setCurrentId} />
-                    </Grid>
-                ))}
-            </Grid>
-            )
+        <div style={{ position: 'relative' }}>
+            <Hidden xsDown>
+                <div style={{ position: 'absolute', top: '0', left: '0' }}>
+                    {!posts.length ?
+                        <div className={classes.quoteWrapper}>
+                            <Typography className={classes.quote} variant='h3' align='center'>"Art enables us to find ourselves and lose ourselves at the same time.” – Thomas Merton</Typography>
+                        </div> :
+                        <div className={classes.elementToFadeInAndOut}>
+                            <div className={classes.quoteWrapper}>
+                                <Typography className={classes.quote} variant='h3' align='center'>"Art enables us to find ourselves and lose ourselves at the same time.” – Thomas Merton</Typography>
+                            </div>
+                        </div>
+                    }
+                </div>
+            </Hidden>
+            <Fade in timeout={35000}>
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post} setCurrentId={setCurrentId} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Fade>
+        </div>
     )
 }
 
--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -12,12 +12,20 @@ export default makeStyles((theme) => ({
     textAlign: 'center',
   },
   quoteWrapper: {
-    background: 'beige',
+    background: 'rgba(219, 210, 178, 1)',
     padding: '50px',
+    margin: '15px 0',
   },
   quote: {
-    // color: 'rgba(163,9,22, 1)',
     color: 'black',
     fontFamily: 'Marker Felt',
   },
+  elementToFadeInAndOut: {
+    opacity: '0',
+    animation: '$fade 6s linear',
+  },
+  "@keyframes fade": {
+    "0%": { opacity: 1 },
+    "100%": { opacity: 0 },
+  }
 }));
